01ccfc230418b58deb0c1b8fb6d1ac12529cdc18
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2022-09-15 14:29:14 -0700

    start project

---
+++ app/src/main/java/com/xd/camerademokotlinstartercode/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.xd.camerademokotlinstartercode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+    private lateinit var textView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        imageView = findViewById(R.id.imageProfile)
+        textView = findViewById(R.id.text_view)
+
+
+    }
+}
